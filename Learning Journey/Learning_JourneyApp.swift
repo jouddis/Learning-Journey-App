1ad2bb0c3a60dd8117906ca65d071cf6c5641160
joud <joudsba@gmail.com>
2025-10-16 10:27:09 +0300

    Initial Commit

---
+++ Learning Journey/Learning_JourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_JourneyApp.swift
+//  Learning Journey
+//
+//  Created by Joud Almashgari on 16/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Learning_JourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
